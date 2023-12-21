70510b5fc83168675fa336aef25a263ff9f25d48
gsummit09 <nayaksumit998@gmail.com>
2023-12-21 22:33:13 +0530

    Changes have been committed.

---
+++ Phase1Section3.12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section3._12
+{
+    internal class Program
+    {
+        public static void doApp()
+        {
+            string dir =Directory.GetCurrentDirectory();
+            Console.WriteLine(dir);
+            string fileName = dir + "data.txt";
+            if(File.Exists(fileName) ) 
+            {
+                Console.WriteLine(fileName + " exists");
+            }
+            else
+            {
+                Console.WriteLine(fileName+" does not exist");
+            }
+            using(StreamWriter sw = new StreamWriter(fileName)) 
+            {
+                sw.WriteLine(" This is line one");
+                sw.WriteLine(" This is line Two");
+                sw.WriteLine(" This is line Three");
+                sw.WriteLine(" This is line four");
+
+            }
+            Console.WriteLine(fileName + " created");
+            string[] lines = File.ReadAllLines(fileName);
+            foreach(string s in lines )
+            {
+                Console.WriteLine(s);
+            }
+            string text = File.ReadAllText(fileName);
+            Console.WriteLine(text);
+            File.Delete(fileName);
+            Console.WriteLine(fileName +" was deleted");
+        }
+        static void Main(string[] args)
+        {
+            doApp();
+        }
+    }
+}
